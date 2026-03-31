5a7de7f2b794f5c31f1c61c789e9548da778ae68
BlackCumin <kostik.kocherga@gmail.com>
2026-03-31 21:15:32 +0300

    Refactoring

--- Weather/csv_plot_weather.py
+++ Weather/csv_plot_weather.py
@@ -3,50 +3,50 @@ import sys
 from tkinter import Tk, filedialog 
 from weather import Weather
 
+def main():
+    root = Tk()
 
-root = Tk()
+    root.withdraw()
 
-root.withdraw()
+    def choose_file():
+        filename = filedialog.askopenfilename(
+            title="Choose CSV file",
+        filetypes=(("CSV files", "*.csv"), ("All files", "*.*"))
+        )
+        
+        if not filename:
+            sys.exit()
+        
+        return filename
 
-def choose_file():
-    filename = filedialog.askopenfilename(
-        title="Choise CSV file",
-    initialdir="Weather/data",
-    filetypes=(("CSV files", "*.csv"), ("All files", "*.*"))
-    )
+    def open_file():
+        filename = choose_file()
+        weather = Weather(filename)
+        return weather
+
+
+    weather = open_file()
     
-    if not filename:
-        return None
+    while True:
+        print('Enter "q" if you want to exit\n'
+            'Please choose the type:\n'
+            'Temperature = 1\nPrecipitation = 2\nAnother file = 3')
+        question = input('')
+        if question == '1':
+            dates, highs, lows, year = weather.get_temperature_data()
+            weather.plot_temperature(dates, year, highs, lows)
+        elif question == '2':
+            dates, precipitations, year = weather.get_precipitation_data()
+            weather.plot_precipitation(dates, year, precipitations)
+        elif question == '3':
+            weather = open_file()
+        elif question == 'q':
+            break
+        else:
+            print('Unknown command. Enter 1, 2, 3 or q.')
+            continue
     
-    return filename
 
-filename = choose_file()
-
-weather = Weather(filename)
-
-if filename is None:
-    print("File not selected")
-    sys.exit()
-
-
-while True:
-    print('Enter "q" if you want to exit\n'
-          'Please choise the type:\n'
-          'Temperature = 1\nPrecipitation = 2')
-    # Add "Another file = 3"
-    question = input('')
-    if question == '1':
-        weather.high_and_low_temperature()
-        weather.plot('temperature')
-    elif question == '2':
-        weather.get_precipitation()
-        weather.plot('precipitation')
-    elif question == 'q':
-        break
-    else:
-        print('Only numbers!')
-        continue
-
-
-    
-    
+        
+if __name__ == '__main__':
+    main()
--- Weather/weather.py
+++ Weather/weather.py
@@ -3,20 +3,16 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt 
 
-class Weather():
+class Weather:
     
     def __init__(self, filename):
-        self.dates = []
-        self.precipitations = []
-        self.highs = [] 
-        self.lows = []
         self.filename = filename
-        self.open_file()
+        self.read_file()
         
-    def open_file(self):
-        with open(self.filename) as f:
-            self.reader = csv.reader(f)
-            header_row = next(self.reader)
+    def read_file(self):
+        with open(self.filename, newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            header_row = next(reader)
                 
             self.prcp_index = header_row.index('PRCP')
             self.name_index = header_row.index('NAME')
@@ -24,63 +20,78 @@ class Weather():
             self.tmax_index = header_row.index('TMAX')
             self.tmin_index = header_row.index('TMIN')
             
-            self.rows = list(self.reader)
+            self.rows = list(reader)
+            if not self.rows:
+                raise ValueError('CSV file contains no data rows.')
+            self.place_name = self.rows[0][self.name_index]
             
-    def high_and_low_temperature(self):
-        self.dates = []
-        self.highs = []
-        self.lows = []
-        for self.row in self.rows:
-            current_data = datetime.strptime(self.row[self.date_index], "%Y-%m-%d")
-            self.year = current_data.year
+                
+    def get_temperature_data(self):
+        dates = []
+        highs = []
+        lows = []
+        year = None
+        
+        for row in self.rows:
+            current_date = datetime.strptime(row[self.date_index], "%Y-%m-%d")
+            year = current_date.year
             try:
-                high = int(self.row[self.tmax_index])
-                low = int(self.row[self.tmin_index])
+                high = int(row[self.tmax_index])
+                low = int(row[self.tmin_index])
             except ValueError:
-                print(f'Missing data for {current_data}')
+                print(f'Missing data for {current_date}')
             else:
-                self.dates.append(current_data)
-                self.highs.append(high)
-                self.lows.append(low)
-            
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
+        if not dates:
+            raise ValueError('No valid temperature data found.')
+        return dates, highs, lows, year
 
-    def get_precipitation(self):
-        self.dates = []
-        self.precipitations = []
-        for self.row in self.rows:
-            current_data = datetime.strptime(self.row[self.date_index], "%Y-%m-%d")
-            self.year = current_data.year
+    def get_precipitation_data(self):
+        dates = []
+        precipitations = []
+        year = None
+        
+        for row in self.rows:
+            current_date = datetime.strptime(row[self.date_index], "%Y-%m-%d")
+            year = current_date.year
             try:
-                precipitation = float(self.row[self.prcp_index])
+                precipitation = float(row[self.prcp_index])
             except ValueError:
-                print(f'Missing data for {current_data}')
+                print(f'Missing data for {current_date}')
             else:
-                self.dates.append(current_data)
-                self.precipitations.append(precipitation)
+                dates.append(current_date)
+                precipitations.append(precipitation)
+        if not dates:
+            raise ValueError('No valid precipitation data found.')
+        return dates, precipitations, year
         
         
-        
-    def plot(self, mode):
-        
+    def plot_temperature(self, dates, year, highs, lows):
+        plt.style.use('classic')
+        fig, ax = plt.subplots() 
+        ax.plot(dates, highs, c='red', alpha=0.5)
+        ax.plot(dates, lows, c='blue', alpha=0.5)
+        title = f'Annual high and low temperatures - {year}\n{self.place_name}'
+        ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+        ax.set_ylabel('Temperature (F)', fontsize=16)
+        ax.set_title(title, fontsize=20)
+        ax.set_xlabel('', fontsize=16)
+        ax.tick_params(axis='both', which='major', labelsize=16)
+        fig.autofmt_xdate()
+        plt.show()    
+            
+            
+    def plot_precipitation(self, dates, year, precipitations):
         plt.style.use('classic')
         fig, ax = plt.subplots()
-        
-        if mode == 'temperature':
-            ax.plot(self.dates, self.highs, c='red', alpha=0.5)
-            plt.plot(self.dates, self.lows, c='blue', alpha=0.5)
-            title = f'Annual high and low temperatures - {self.year}\n{self.row[self.name_index]}'
-            plt.fill_between(self.dates, self.highs, self.lows, facecolor='blue', 
-                            alpha=0.1)
-            plt.ylabel('Temperature (F)', fontsize=16)
-            
-        elif mode == 'precipitation':
-            ax.plot(self.dates, self.precipitations, c='blue')
-            title = f'Annual precipitations - {self.year}\n{self.row[self.name_index]}'
-            plt.ylabel('Precipitations', fontsize=16)
-
-        plt.title(title, fontsize=20)
-        plt.xlabel('', fontsize=16)
-        plt.tick_params(axis='both', which='major', labelsize=16)
+        ax.plot(dates, precipitations, c='blue')
+        title = f'Annual precipitations - {year}\n{self.place_name}'
+        ax.set_ylabel('Precipitations', fontsize=16)
+        ax.set_title(title, fontsize=20)
+        ax.set_xlabel('', fontsize=16)
+        ax.tick_params(axis='both', which='major', labelsize=16)
         fig.autofmt_xdate()
         plt.show()
         
