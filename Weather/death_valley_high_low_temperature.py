35b7055417d6adcc1eb87b23f6e67425a7f0dfad
BlackCumin <kostik.kocherga@gmail.com>
2026-03-31 10:48:21 +0300

    Add San Francisco weather

---
+++ Weather/death_valley_high_low_temperature.py
@@ -0,0 +1,41 @@
+import csv
+
+from matplotlib import pyplot as plt
+from datetime import datetime 
+
+filename = 'Weather/data/death_valley_2021_full.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+        
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_data = datetime.strptime(row[2], "%Y-%m-%d")
+        try:
+            high = int(row[7])
+            low = int(row[8])
+        except ValueError:
+            print(f'Missing data for {current_data}')
+        else:
+            dates.append(current_data)
+            highs.append(high)
+            lows.append(low)
+            
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.plot(dates, highs, c='red', alpha=0.5)
+    ax.set_ylim(0, 120)
+    plt.plot(dates, lows, c='blue', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+    
+    title = 'Daily high and low temperatures - 2021\nDeath Valey, CA'
+    plt.title(title, fontsize=20)
+    plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel('Temperature (F)', fontsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=16)
+    
+    plt.show()
--- Weather/death_valley_precipitation.py
+++ Weather/death_valley_precipitation.py
@@ -3,7 +3,7 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime 
 
-filename = 'Weather/data/death_valley_2021_simple.csv'
+filename = 'Weather/data/death_valley_2021_full.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -11,30 +11,27 @@ with open(filename) as f:
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
         
-    dates, highs, lows = [], [], []
+    dates, precipitations = [], []
     for row in reader:
         current_data = datetime.strptime(row[2], "%Y-%m-%d")
         try:
-            high = int(row[4])
-            low = int(row[5])
+            precipitation = float(row[5])
         except ValueError:
             print(f'Missing data for {current_data}')
         else:
             dates.append(current_data)
-            highs.append(high)
-            lows.append(low)
+            precipitations.append(precipitation)
+
             
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.plot(dates, highs, c='red', alpha=0.5)
-    plt.plot(dates, lows, c='blue', alpha=0.5)
-    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+    ax.plot(dates, precipitations, c='blue')
+    title = 'Daily precipitations - 2021\nDeath Valley, CA'
     
-    title = 'Daily high and low temperatures - 2021\nDeath Valley, CA'
     plt.title(title, fontsize=20)
     plt.xlabel('', fontsize=16)
     fig.autofmt_xdate()
-    plt.ylabel('Temperature (F)', fontsize=16)
+    plt.ylabel('Precipitations', fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
     
     plt.show()
